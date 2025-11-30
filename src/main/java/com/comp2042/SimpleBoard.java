a79ba56f43d637cf1634b206461b947e5e487c1b
AtifAsrul <109678476+AtifAsrul@users.noreply.github.com>
2025-11-30 20:23:06 +0800

    Fix: Removed magic values on ln 70 and used constant vars

--- src/main/java/com/comp2042/SimpleBoard.java
+++ src/main/java/com/comp2042/SimpleBoard.java
@@ -67,7 +67,7 @@ public class SimpleBoard implements Board {
     public boolean createNewBrick() {
         Brick currentBrick = brickGenerator.getBrick();
         brickRotator.setBrick(currentBrick);
-        currentOffset = new Point(4, 10);
+        currentOffset = new Point(Constants.START_X, Constants.START_Y);
         return MatrixOperations.intersect(currentGameMatrix, brickRotator.getCurrentShape(), (int) currentOffset.getX(), (int) currentOffset.getY());
     }
 
